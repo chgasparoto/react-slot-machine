23a63111566f5736ffd350916c8b6459c637ce76
Cleber Gasparoto <chgasparoto@gmail.com>
2019-04-07 20:33:53 -0300

    Updates

---
+++ precache-manifest.72f33a6a43246b336a2407adcf48bdfc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b3e3f1cf53fb9774f28665ce2fd60d3",
+    "url": "/react-slot-machine/static/media/orange.7b3e3f1c.png"
+  },
+  {
+    "revision": "65252ba2a0e95d47e228",
+    "url": "/react-slot-machine/static/css/main.7804653c.chunk.css"
+  },
+  {
+    "revision": "6bf016dda2be656d64ed",
+    "url": "/react-slot-machine/static/js/runtime~main.008f5136.js"
+  },
+  {
+    "revision": "2d82d5a9c75c3751f386",
+    "url": "/react-slot-machine/static/js/2.b5b04a53.chunk.js"
+  },
+  {
+    "revision": "d77cb1779a8aeb0701ad5841d064a369",
+    "url": "/react-slot-machine/static/media/strawberry.d77cb177.png"
+  },
+  {
+    "revision": "abf2234f9a3923581ee469a9fdacc34d",
+    "url": "/react-slot-machine/static/media/banana.abf2234f.png"
+  },
+  {
+    "revision": "65252ba2a0e95d47e228",
+    "url": "/react-slot-machine/static/js/main.21f7baf7.chunk.js"
+  },
+  {
+    "revision": "0208eeabaf464ef48734afc7213356f0",
+    "url": "/react-slot-machine/static/media/monkey.0208eeab.png"
+  },
+  {
+    "revision": "5419a33c5eeffab8be06e5635449b00c",
+    "url": "/react-slot-machine/static/media/win.5419a33c.wav"
+  },
+  {
+    "revision": "05c73073813b23e27dfc90b2f42fb5d1",
+    "url": "/react-slot-machine/static/media/fail.05c73073.wav"
+  },
+  {
+    "revision": "e0f2b0be83f7728e712e9e489a707b07",
+    "url": "/react-slot-machine/static/media/coin.e0f2b0be.wav"
+  },
+  {
+    "revision": "49ec3bb44f2ebd3b6cfc6d3aa476d030",
+    "url": "/react-slot-machine/index.html"
+  }
+];
